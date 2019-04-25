0b5e0f230c57679ff34b65e90f2908c5ca0223dd
Noor-ul-Anam <anam.xyed97@gmail.com>
2019-04-26 01:28:46 +0500

    update function and some bugs

--- js/js.js
+++ js/js.js
@@ -9,28 +9,31 @@ function renderTasks() {
   let element = document.getElementById('tasks');
   element.innerHTML = '';
   for (let i = 0; i < taskArr.length; i++) {
-    element.innerHTML += "<div class='task'>" +`<p>${taskArr[i]}</p>` + `<input class='insertBtn'type='image' src='./asset/download.png' onclick=' insertTask(${i})'>`+`<input class='completeBtn'type='image' src='./asset/download (1).png' onclick='finishEdit()' >` + `<button class='btn' onclick='deleteTask(${i})'>x</button>` + "</div>";
+    element.innerHTML += "<div class='task'>" +`<p>${taskArr[i]}</p>` + `<input class='insertBtn'type='image' src='./asset/download.png' onclick=' insertTask(${i})'>`+`<input class='completeBtn'type='image' src='./asset/download (1).png' onclick='updateTask(${i})' >` + `<button class='btn' onclick='deleteTask(${i})'>x</button>` +  "</div>";
+  document.getElementById('txtInp').value = '';
   }
-  document.getElementById('txtInp').value = ''; 
+   
 }
 renderTasks();
+
+function updateTask(i){
+  let temp =document.getElementsByTagName('p').item(i);
+  temp.contentEditable = false; 
+  taskArr = taskArr.map((value,index) => {
+    if(index === i){ 
+      return temp.innerText;
+    }else{
+      return value;
+    }
+  })
+  console.log(taskArr);
+}
 
 function insertTask(i){
   document.getElementsByTagName('p').item(i).contentEditable = 'true';
   document.getElementsByTagName('p').item(i).focus();
-  updateTaskArr = (i) => {
-    taskArr = taskArr.map((value,index) => {
-      if(index===i){
-        return document.getElementsByTagName('p').item(i).innerText;
-      }else{
-        return value;
-      }
-    });
-  }
 }
-function finishEdit(i){
-  document.getElementsByTagName('p').item(i).contentEditable = false;  
-}
+
 function deleteTask(i){
   taskArr = taskArr.filter((value,itsIndex) => {
     if (itsIndex === i) {
