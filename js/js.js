a25cd89d6c7507498898121dceaa66efa1879738
Noor-ul-Anam <anam.xyed97@gmail.com>
2019-04-29 02:23:20 +0500

    Local storage feature added

--- js/js.js
+++ js/js.js
@@ -1,23 +1,32 @@
-var taskArr = ['aaa'];
+var taskArr = [];
 
 function onChangeHandler(e) {
   taskArr = [e.value, ...taskArr];
+  setTask();
   renderTasks();
+  console.log(taskArr);
+}
+
+let getTask = () => {
+  // taskArr= JSON.parse(window.localStorage/getItem('Task'));
+  taskArr =JSON.parse(window.localStorage.getItem('Task'));
+}
+
+let setTask = () => {
+  window.localStorage.setItem('Task',JSON.stringify(taskArr));
 }
 
 function renderTasks() { 
+  getTask();
+  setTask();
   let element = document.getElementById('tasks');
   element.innerHTML = '';
   for (let i = 0; i < taskArr.length; i++) {
     element.innerHTML += "<div class='task'>" +`<p>${taskArr[i]}</p>` +`<a class='completeBtn' tooltip='Complete' onclick='updateTask(${i})'><img src='./asset/download (1).png'></a>` + `<a class='insertBtn btn' src='./asset/download.png' tooltip='Edit'onclick='insertTask(${i})'><img src='./asset/download.png'></a>`+ `<a class='deleteBtn' tooltip='Delete' onclick='deleteTask(${i})'><img src='./asset/download (2).png'></a>`+ "</div>";
-    document.getElementById('txtInp').value = '';
+    document.getElementById('txtInp').value = ''; 
   }
 }
 renderTasks();
-/*<span class='tooltip'>Finish</span>*/
-function tooltip(){
-
-}
 
 function updateTask(i){
   let temp =document.getElementsByTagName('p').item(i);
@@ -29,7 +38,7 @@ function updateTask(i){
       return value;
     }
   })
-  console.log(taskArr);
+  setTask();
 }
 
 function insertTask(i){
@@ -45,5 +54,6 @@ function deleteTask(i){
       return true;
     }
   });
+  setTask();
   renderTasks();
 }
